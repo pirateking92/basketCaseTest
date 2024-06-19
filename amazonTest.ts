a9b7db0940a1ff10bf91267d74096707fae4229c
Matt Doyle <doyle9214@gmail.com>
2024-06-19 14:23:19 +0100

    made separate utils file and test class so as the make the testing more modular, easier to maintain and reduce duplicate code

--- amazonTest.ts
+++ amazonTest.ts
@@ -1,90 +1,112 @@
 import { Builder, By, until, WebDriver } from "selenium-webdriver";
+import { handleCookieConsent } from "./utils";
 
-async function amazonTest() {
-  let driver: WebDriver = await new Builder().forBrowser("firefox").build();
+// changing the test to be encapsulated in a class
+// will make it easier to read and will make it so that
 
-  try {
-    await driver.get("https://www.amazon.co.uk");
+export class AmazonTest {
+  private driver: WebDriver;
 
-    // Handle cookie consent if present
-    await handleCookieConsent(driver);
-
-    await searchProduct(driver, "nalgene");
-    await selectFirstProduct(driver);
-    await addToCart(driver);
-    await proceedToCheckout(driver);
-    await verifyCheckoutPage(driver);
-  } catch (error) {
-    console.error("Test failed:", error);
-  } finally {
-    await driver.quit();
+  constructor() {
+    this.driver = new Builder().forBrowser("chrome").build();
   }
-}
 
-// divide sections into different functions to improve readability and future changes
-// cookies function
-async function handleCookieConsent(driver: WebDriver) {
-  try {
-    await driver.wait(until.elementLocated(By.id("sp-cc-accept")), 5000);
-    let cookieConsentButton = await driver.findElement(By.id("sp-cc-accept"));
-    await cookieConsentButton.click();
-  } catch (error) {
-    console.log("Cookie consent not found, continuing.");
+  async runTest() {
+    try {
+      await this.driver.get("https://www.amazon.co.uk");
+
+      // checks all functions below. much cleaner than before
+      await handleCookieConsent(this.driver, "sp-cc-accept");
+      await this.searchProduct("nalgene");
+      await this.selectFirstProduct();
+      await this.addToCart();
+      await this.proceedToCheckout();
+      await this.verifyCheckoutPage();
+    } catch (error) {
+      console.error("Test failed:", error);
+    } finally {
+      await this.driver.quit();
+    }
   }
-}
+  // made utils file to handle some of these functions. test still passes
 
-// search function
-async function searchProduct(driver: WebDriver, productName: string) {
-  await driver.wait(until.elementLocated(By.name("field-keywords")), 10000);
-  let searchBar = await driver.findElement(By.name("field-keywords"));
-  await searchBar.sendKeys(productName);
-  await searchBar.submit();
-}
+  // divide sections into different functions to improve readability and future changes
+  // cookies function
+  // private async handleCookieConsent(idInput: string) {
+  //   try {
+  //     await this.driver.wait(until.elementLocated(By.id(idInput)), 3000);
+  //     let cookieConsentButton = await this.driver.findElement(
+  //       By.id("sp-cc-accept")
+  //     );
+  //     await cookieConsentButton.click();
+  //   } catch (error) {
+  //     console.log("Cookie consent not found, continuing.");
+  //   }
+  // }
 
-// function for selecting first product
-async function selectFirstProduct(driver: WebDriver) {
-  await driver.wait(
-    until.elementLocated(
+  // search function
+  private async searchProduct(productName: string) {
+    await this.driver.wait(
+      until.elementLocated(By.name("field-keywords")),
+      3000
+    );
+    let searchBar = await this.driver.findElement(By.name("field-keywords"));
+    await searchBar.sendKeys(productName);
+    await searchBar.submit();
+  }
+
+  // function for selecting first product
+  private async selectFirstProduct() {
+    await this.driver.wait(
+      until.elementLocated(
+        By.css(
+          "div.s-main-slot.s-result-list.s-search-results.sg-row > div[data-index='3']"
+        )
+      ),
+      10000
+    );
+    let firstItem = await this.driver.findElement(
       By.css(
         "div.s-main-slot.s-result-list.s-search-results.sg-row > div[data-index='3']"
       )
-    ),
-    10000
-  );
-  let firstItem = await driver.findElement(
-    By.css(
-      "div.s-main-slot.s-result-list.s-search-results.sg-row > div[data-index='3']"
-    )
-  );
-  await firstItem.click();
-}
+    );
+    await firstItem.click();
+  }
 
-// last comment becuase it makes sense what they do haha
-async function addToCart(driver: WebDriver) {
-  await driver.wait(until.elementLocated(By.id("add-to-cart-button")), 10000);
-  let addToCartButton = await driver.findElement(By.id("add-to-cart-button"));
-  await addToCartButton.click();
-}
+  // add to cart function. will press the add to cart button
+  private async addToCart() {
+    await this.driver.wait(
+      until.elementLocated(By.id("add-to-cart-button")),
+      3000
+    );
+    let addToCartButton = await this.driver.findElement(
+      By.id("add-to-cart-button")
+    );
+    await addToCartButton.click();
+  }
 
-async function proceedToCheckout(driver: WebDriver) {
-  await driver.wait(
-    until.elementLocated(By.name("proceedToRetailCheckout")),
-    10000
-  );
-  let goToCheckoutButton = await driver.findElement(
-    By.name("proceedToRetailCheckout")
-  );
-  await goToCheckoutButton.click();
-}
+  // proceed to checkout, will press the proceed to checkout button
+  private async proceedToCheckout() {
+    await this.driver.wait(
+      until.elementLocated(By.name("proceedToRetailCheckout")),
+      3000
+    );
+    let goToCheckoutButton = await this.driver.findElement(
+      By.name("proceedToRetailCheckout")
+    );
+    await goToCheckoutButton.click();
+  }
 
-async function verifyCheckoutPage(driver: WebDriver) {
-  await driver.wait(until.urlContains("/ap/signin?_"), 10000);
-  let currentUrl = await driver.getCurrentUrl();
-  if (currentUrl.includes("/ap/signin?_")) {
-    console.log("Test passed: Navigated to checkout.");
-  } else {
-    console.log("Test failed: Did not navigate to checkout.");
+  // verifies that after proceeding to the checkout, that the correct page has been accessed.
+  // at this point it checks if the signin page is accessed as the user would have to login at this point
+  private async verifyCheckoutPage() {
+    await this.driver.wait(until.urlContains("/ap/signin?_"), 3000);
+    let currentUrl = await this.driver.getCurrentUrl();
+    if (currentUrl.includes("/ap/signin?_")) {
+      console.log("Test passed: Navigated to checkout.");
+    } else {
+      console.log("Test failed: Did not navigate to checkout.");
+    }
   }
 }
-
-amazonTest();
+//could add extra function that logs in. could add some credentials to a .env to load.
---
+++ testRunner.ts
@@ -0,0 +1,3 @@
+import { AmazonTest } from "./amazonTest";
+const test = new AmazonTest();
+test.runTest();
